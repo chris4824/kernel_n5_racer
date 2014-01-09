0590ac6d3a9eadfea0e501588818f2f0a3530f8e
Paul Reioux <reioux@gmail.com>
2014-01-08 19:19:24 -0800

    mm/page_io.c: remove printk warning when used with zswap and fix build warning
    mm/page_io.c: remove printk warning when used with zswap
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    mm/page_io.c: fix build warning - remove unused variable

--- mm/page_io.c
+++ mm/page_io.c
@@ -53,8 +53,6 @@ static void end_swap_bio_write(struct bio *bio, int err)
 {
 	const int uptodate = test_bit(BIO_UPTODATE, &bio->bi_flags);
 	struct page *page = bio->bi_io_vec[0].bv_page;
-	static unsigned long swap_error_rs_time;
-
 	if (!uptodate) {
 		SetPageError(page);
 		/*
@@ -66,12 +64,13 @@ static void end_swap_bio_write(struct bio *bio, int err)
 		 * Also clear PG_reclaim to avoid rotate_reclaimable_page()
 		 */
 		set_page_dirty(page);
-		if (printk_timed_ratelimit(&swap_error_rs_time,
-					   SWAP_ERROR_LOG_RATE_MS))
-			printk(KERN_ALERT "Write-error on swap-device (%u:%u:%Lu)\n",
+
+#ifndef CONFIG_ZSWAP
+		printk(KERN_ALERT "Write-error on swap-device (%u:%u:%Lu)\n",
 				imajor(bio->bi_bdev->bd_inode),
 				iminor(bio->bi_bdev->bd_inode),
 				(unsigned long long)bio->bi_sector);
+#endif
 		ClearPageReclaim(page);
 	}
 	end_page_writeback(page);
